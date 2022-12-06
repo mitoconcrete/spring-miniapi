744d3accd9a7e1a18f1d4db0d2a415eb39e6e9a5
mitoconcrete <xogns98@gmail.com>
2022-12-06 16:54:26 +0900

    feat] exception when not exist data request.

--- src/main/java/com/api/post/controller/PostController.java
+++ src/main/java/com/api/post/controller/PostController.java
@@ -6,8 +6,10 @@ import com.api.post.entity.Post;
 import com.api.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.http.HttpResponse;
 import java.util.List;
 
 @RestController
@@ -25,18 +27,30 @@ public class PostController {
     }
 
     @GetMapping("/api/posts/{id}")
-    public PostResponseDto getPost(@PathVariable Long id){
-        return new PostResponseDto(postService.getPost(id));
+    public ResponseEntity getPost(@PathVariable Long id){
+        try{
+        return new ResponseEntity(new PostResponseDto(postService.getPost(id)), HttpStatus.OK);
+        }catch (IllegalArgumentException e){
+            return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
     @PutMapping("/api/posts/{id}")
-    public PostResponseDto updatePost(@PathVariable Long id, @RequestBody PostRequestDto postRequestDto){
-        return new PostResponseDto(postService.updatePost(id, postRequestDto));
+    public ResponseEntity updatePost(@PathVariable Long id, @RequestBody PostRequestDto postRequestDto){
+        try{
+            return new ResponseEntity(new PostResponseDto(postService.updatePost(id, postRequestDto)), HttpStatus.OK);
+        }catch (IllegalArgumentException e){
+            return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
     @DeleteMapping("/api/posts/{id}")
     public HttpStatus deletePost(@PathVariable Long id){
-        postService.deletePost(id);
-        return HttpStatus.OK;
+        try{
+            postService.deletePost(id);
+            return HttpStatus.OK;
+        }catch (IllegalArgumentException e){
+            return HttpStatus.BAD_REQUEST;
+        }
     }
 }
--- src/main/java/com/api/post/service/PostService.java
+++ src/main/java/com/api/post/service/PostService.java
@@ -4,7 +4,9 @@ import com.api.post.dto.PostRequestDto;
 import com.api.post.entity.Post;
 import com.api.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -15,26 +17,26 @@ public class PostService {
     private final PostRepository postRepository;
 
     @Transactional(readOnly = true)
-    public List<Post> getPosts(){
+    public List<Post> getPosts() {
         return postRepository.findAllByOrderByModifiedAtDesc();
     }
 
     @Transactional
-    public Post createPost(PostRequestDto postRequestDto){
+    public Post createPost(PostRequestDto postRequestDto) {
         Post post = new Post(postRequestDto);
         postRepository.save(post);
         return post;
     }
 
     @Transactional(readOnly = true)
-    public Post getPost(Long id){
+    public Post getPost(Long id) {
         return postRepository.findById(id).orElseThrow(
                 () -> new IllegalArgumentException("게시물이 없습니다.")
         );
     }
 
     @Transactional
-    public Post updatePost(Long id, PostRequestDto postRequestDto){
+    public Post updatePost(Long id, PostRequestDto postRequestDto) {
         Post post = postRepository.findByIdAndPassword(id, postRequestDto.getPassword()).orElseThrow(
                 () -> new IllegalArgumentException("해당되는 게시물이 존재하지 않습니다."));
         post.update(postRequestDto);
@@ -43,7 +45,9 @@ public class PostService {
     }
 
     @Transactional
-    public void deletePost(Long id){
-        postRepository.deleteById(id);
+    public void deletePost(Long id) {
+        Post post = postRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당되는 게시물이 존재하지 않습니다."));
+        postRepository.delete(post);
     }
 }
