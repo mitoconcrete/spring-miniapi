bf70570ea3a50cf9d86ea70533cbef0f235e37cd
mitoconcrete <xogns98@gmail.com>
2022-12-06 11:55:55 +0900

    feat: inherit timestamped for audit entity change.

--- src/main/java/com/api/post/entity/Post.java
+++ src/main/java/com/api/post/entity/Post.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @NoArgsConstructor
-public class Post {
+public class Post extends Timestamped{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
