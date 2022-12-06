818677338e6e8f88e0eea338e3e3451f72c45014
mitoconcrete <xogns98@gmail.com>
2022-12-06 11:28:51 +0900

    add] project init.

---
+++ src/main/java/com/api/post/SpringMiniapiApplication.java
@@ -0,0 +1,13 @@
+package com.api.post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMiniapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMiniapiApplication.class, args);
+    }
+
+}
