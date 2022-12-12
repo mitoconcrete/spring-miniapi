3748588513356e66d27c513a7593a338a1d15f5a
mitoconcrete <xogns98@gmail.com>
2022-12-12 16:23:44 +0900

    fix: delete return void only send status code.

--- src/main/java/com/api/post/controller/PostController.java
+++ src/main/java/com/api/post/controller/PostController.java
@@ -15,41 +15,28 @@ import java.util.List;
 public class PostController {
     private final PostService postService;
     @GetMapping("/posts")
-    public ResponseEntity<Object> getPosts(){
-        return
-                ResponseEntity.ok(postService.getPosts());
+    public List<PostResponseDto> getPosts(){
+        return postService.getPosts();
     }
 
     @PostMapping("/posts")
-    public ResponseEntity<Object> createPost(@RequestBody PostRequestDto postRequestDto){
-        return ResponseEntity.ok(new PostResponseDto(postService.createPost(postRequestDto)));
+    @ResponseStatus(HttpStatus.CREATED)
+    public PostResponseDto createPost(@RequestBody PostRequestDto postRequestDto){
+        return postService.createPost(postRequestDto);
     }
 
     @GetMapping("/posts/{id}")
-    public ResponseEntity<Object> getPost(@PathVariable Long id){
-        try{
-        return ResponseEntity.ok(new PostResponseDto(postService.getPost(id)));
-        }catch (IllegalArgumentException e){
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+    public PostResponseDto getPost(@PathVariable Long id){
+        return postService.getPost(id);
     }
 
     @PutMapping("/posts/{id}")
-    public ResponseEntity<Object> updatePost(@PathVariable Long id, @RequestBody PostRequestDto postRequestDto){
-        try{
-            return ResponseEntity.ok(new PostResponseDto(postService.updatePost(id, postRequestDto)));
-        }catch (IllegalArgumentException | CredentialException e){
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+    public PostResponseDto updatePost(@PathVariable Long id, @RequestBody PostRequestDto postRequestDto){
+        return postService.updatePost(id, postRequestDto);
     }
 
     @DeleteMapping("/posts/{id}")
-    public ResponseEntity<Object> deletePost(@PathVariable Long id, @RequestBody PostRequestDto postRequestDto){
-        try{
-            postService.deletePost(id, postRequestDto);
-            return ResponseEntity.ok("delete complete");
-        }catch (IllegalArgumentException | CredentialException e){
-            return ResponseEntity.badRequest().body("delete fail : " + e.getMessage());
-        }
+    public void deletePost(@PathVariable Long id, @RequestBody PostRequestDto postRequestDto){
+        postService.deletePost(id, postRequestDto);
     }
 }
