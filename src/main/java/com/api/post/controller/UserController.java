cd44e31f3a4f653a4ff0af39ec9a34154fcf48e0
mitoconcrete <xogns98@gmail.com>
2022-12-13 16:27:34 +0900

    feat: create usercontroller and set signup api.

--- src/main/java/com/api/post/controller/UserController.java
+++ src/main/java/com/api/post/controller/UserController.java
@@ -1,2 +1,20 @@
-package com.api.post.controller;public class UserController {
+package com.api.post.controller;
+
+import com.api.post.dto.UserRequestDto;
+import com.api.post.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+    @PostMapping("/signup")
+    public void createUser(@RequestBody UserRequestDto userRequestDto){
+
+    }
 }
