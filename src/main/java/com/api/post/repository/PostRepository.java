d42ae6986fe685fbf5a8830cbcfeadd66f7f1546
mitoconcrete <xogns98@gmail.com>
2022-12-14 19:18:37 +0900

    clean] uselsss clean.

--- src/main/java/com/api/post/repository/PostRepository.java
+++ src/main/java/com/api/post/repository/PostRepository.java
@@ -1,15 +1,12 @@
 package com.api.post.repository;
 
 import com.api.post.entity.Post;
-import com.api.post.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByOrderByModifiedAtDesc();
-    Optional<Post> findByIdAndUser(Long postId, User user);
 }
