42953075c5464394ea87340de8946ce85445a9d8
mitoconcrete <xogns98@gmail.com>
2022-12-14 11:35:21 +0900

    feat: username, password condition fix.

--- src/main/java/com/api/post/dto/UserRequestDto.java
+++ src/main/java/com/api/post/dto/UserRequestDto.java
@@ -2,20 +2,20 @@ package com.api.post.dto;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.validator.constraints.ConstraintComposition;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 
 @Getter
 @RequiredArgsConstructor
 public class UserRequestDto {
 
-    @Pattern(regexp = "[a-z0-9]{4,10}$", message = "username use alphabet lowercase and numbers. And letter length 4 to 10")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[0-9])[a-z0-9]{4,10}$",
+             message = "The username is combination of alphabet(lowercase), number.\nAnd letter length 4 to 10")
     private final String username;
 
-    @Pattern(regexp = "[a-zA-Z0-9]{8,15}$", message = "password use alphabet and 0-9 numbers. And letter length 8 to 15")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@!%*#?&])[A-Za-z0-9$@!%*#?&]{8,15}$",
+             message = "The password is combination of alphabet, number, special symbols.\nAnd letter length 8 to 15")
     private final String password;
 }
