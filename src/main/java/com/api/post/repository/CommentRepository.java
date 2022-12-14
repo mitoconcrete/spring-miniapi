8ecb1a12b049af143d0d6825b6e54fc9477516b1
mitoconcrete <xogns98@gmail.com>
2022-12-14 17:26:35 +0900

    feat: add query find by commentid and post instance.

--- src/main/java/com/api/post/repository/CommentRepository.java
+++ src/main/java/com/api/post/repository/CommentRepository.java
@@ -1,6 +1,11 @@
 package com.api.post.repository;
 
 import com.api.post.entity.Comment;
+import com.api.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Optional<Comment> findByIdAndPost(Long commentId, Post post);
 }
