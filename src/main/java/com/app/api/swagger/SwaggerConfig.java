6622de9d8ecd13392c8710e692f17f8d6f5d4e7e
mitoconcrete <xogns98@gmail.com>
2022-12-15 11:20:11 +0900

    feat: swagger doc basic config set.

--- src/main/java/com/app/api/swagger/SwaggerConfig.java
+++ src/main/java/com/app/api/swagger/SwaggerConfig.java
@@ -2,21 +2,67 @@ package com.app.api.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@EnableSwagger2
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.app.api.controller"))
+                .build()
+                .groupName("API 1.0.0") // group별 명칭을 주어야 한다.
+                .pathMapping("/")
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false); // 400,404,500 .. 표기를 ui에서 삭제한다.
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("sparta-spring-project")
+                .description("스프링 api 제작 프로젝트 입니다.")
+                .version("1.0.0")
+                .termsOfServiceUrl("")
+                .license("")
+                .licenseUrl("")
+                .build()
+                ;
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
                 .build();
     }
 
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
 }
