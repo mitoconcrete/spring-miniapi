8f0893f4b70ce18ba1dd88d0ddba9318321707ca
mitoconcrete <xogns98@gmail.com>
2022-12-15 16:25:18 +0900

    feat: uesr validation message fix.

--- src/main/java/com/app/api/dto/request/UserRequestDto.java
+++ src/main/java/com/app/api/dto/request/UserRequestDto.java
@@ -11,11 +11,11 @@ import javax.validation.constraints.Pattern;
 public class UserRequestDto {
 
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[0-9])[a-z0-9]{4,10}$",
-             message = "The username is combination of alphabet(lowercase), number.\nAnd letter length 4 to 10")
+             message = "4자 이상 10자 이내여야하며, 하나 이상의 알파벳 소문자와 숫자의 조합으로 이뤄져야 합니다.")
     private final String username;
 
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@!%*#?&])[A-Za-z0-9$@!%*#?&]{8,15}$",
-             message = "The password is combination of alphabet, number, special symbols.\nAnd letter length 8 to 15")
+             message = "8자 이상 15자 이내여야하며, 하나 이상의 알파벳, 숫자, 특수문자의 조합으로 이뤄져야 합니다.")
     private final String password;
     private final UserRoleEnum role;
 }
--- src/main/java/com/app/api/exception/ExceptionController.java
+++ src/main/java/com/app/api/exception/ExceptionController.java
@@ -3,6 +3,8 @@ package com.app.api.exception;
 import com.app.api.dto.response.ExceptionResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,23 +14,37 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionController {
     @ExceptionHandler(IllegalArgumentException.class)
-    public ExceptionResponseDto IllegalArgumentException(Exception e){
+    public ExceptionResponseDto IllegalArgumentException(IllegalArgumentException e){
         return new ExceptionResponseDto(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ExceptionResponseDto HttpMessageNotReadableException(Exception e){
+    public ExceptionResponseDto HttpMessageNotReadableException(HttpMessageNotReadableException e){
         return new ExceptionResponseDto(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public ExceptionResponseDto HttpRequestMethodNotSupportedException(Exception e){
+    public ExceptionResponseDto HttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e){
         return new ExceptionResponseDto(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ExceptionResponseDto MethodArgumentNotValidException(Exception e){
-        return new ExceptionResponseDto(HttpStatus.BAD_REQUEST, e.getMessage());
+    public ExceptionResponseDto MethodArgumentNotValidException(MethodArgumentNotValidException e){
+        System.out.println(e.getBindingResult());
+        BindingResult bindingResult = e.getBindingResult();
+        StringBuilder builder = new StringBuilder();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            builder.append("[");
+            builder.append(fieldError.getField());
+            builder.append("](은)는 ");
+            builder.append(fieldError.getDefaultMessage());
+            builder.append(" 입력된 값: [");
+            builder.append(fieldError.getRejectedValue());
+            builder.append("]");
+            builder.append(" / ");
+        }
+
+        return new ExceptionResponseDto(HttpStatus.BAD_REQUEST, builder.deleteCharAt(builder.lastIndexOf(" / ")).toString());
     }
 
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
