4ab69b3051f48198a542a9ab98a8c6c0e2e4a920
mitoconcrete <xogns98@gmail.com>
2022-12-15 20:08:51 +0900

    feat: make refreshtoken entity.

---
+++ src/main/java/com/app/api/entity/UserRefreshToken.java
@@ -0,0 +1,24 @@
+package com.app.api.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@RequiredArgsConstructor
+public class UserRefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String token;
+
+    @OneToOne
+    private User user;
+
+    public boolean isTokenValid(String token){
+        return this.token.equals(token);
+    }
+}
