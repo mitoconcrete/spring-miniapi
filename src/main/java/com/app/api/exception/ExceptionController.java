6c47ef3003b4c3a4fb7fd5c88d64213ec3f0bab8
mitoconcrete <xogns98@gmail.com>
2022-12-15 14:59:02 +0900

    feat: error handle when id and password validation fail.

--- src/main/java/com/app/api/exception/ExceptionController.java
+++ src/main/java/com/app/api/exception/ExceptionController.java
@@ -4,6 +4,7 @@ import com.app.api.dto.ExceptionResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -21,6 +22,11 @@ public class ExceptionController {
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ExceptionResponseDto HttpRequestMethodNotSupportedException(Exception e){
+        return new ExceptionResponseDto(HttpStatus.BAD_REQUEST, e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ExceptionResponseDto MethodArgumentNotValidException(Exception e){
         return new ExceptionResponseDto(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 }
