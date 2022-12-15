dc11a863eedde49bf2ce2a840e6aef0340627fdd
mitoconcrete <xogns98@gmail.com>
2022-12-15 14:39:00 +0900

    feat: controllers title and description add.

--- src/main/java/com/app/api/controller/PostController.java
+++ src/main/java/com/app/api/controller/PostController.java
@@ -4,6 +4,7 @@ import com.app.api.dto.PostRequestDto;
 import com.app.api.dto.PostResponseDto;
 import com.app.api.service.PostService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,33 +12,39 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
-@Api(tags = "게시물")
+@Api(tags = "게시글")
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class PostController {
     private final PostService postService;
+
+    @ApiOperation(value = "모든 게시글 조회", notes = "모든 게시글을 조회 할 수 있습니다.<br>게시글에 포함 된 댓글들도 조회합니다.")
     @GetMapping("/posts")
     public List<PostResponseDto> getPosts(){
         return postService.getPosts();
     }
 
+    @ApiOperation(value = "새 게시글 생성", notes = "새로운 게시글을 생성합니다.")
     @PostMapping("/posts")
     @ResponseStatus(HttpStatus.CREATED)
     public PostResponseDto createPost(@RequestBody PostRequestDto postRequestDto, HttpServletRequest request){
         return postService.createPost(postRequestDto, request);
     }
 
+    @ApiOperation(value = "지정된 게시글 조회", notes = "지정된 게시글을 조회 할 수 있습니다.<br>게시글에 포함 된 댓글들도 조회합니다.")
     @GetMapping("/posts/{id}")
     public PostResponseDto getPost(@PathVariable Long id){
         return postService.getPost(id);
     }
 
+    @ApiOperation(value = "지정된 게시글 수정", notes = "지정된 게시글의 내용을 수정합니다.<br>해당 게시물이 자신이 작성한 댓글이 아니라면 수정은 불가능합니다.<br>하지만, 관리자는 모든 게시물을 수정할 수 있습니다.")
     @PutMapping("/posts/{id}")
     public PostResponseDto updatePost(@PathVariable Long id, @RequestBody PostRequestDto postRequestDto, HttpServletRequest request){
         return postService.updatePost(id, postRequestDto, request);
     }
 
+    @ApiOperation(value = "지정된 게시글 삭제", notes = "지정된 게시글의 내용을 삭제합니다.<br>해당 게시물이 자신이 작성한 댓글이 아니라면 삭제는 불가능합니다.<br>하지만, 관리자는 모든 게시물을 삭제할 수 있습니다.")
     @DeleteMapping("/posts/{id}")
     public String deletePost(@PathVariable Long id, HttpServletRequest request){
         postService.deletePost(id, request);
--- src/main/java/com/app/api/controller/UserController.java
+++ src/main/java/com/app/api/controller/UserController.java
@@ -3,6 +3,7 @@ package com.app.api.controller;
 import com.app.api.dto.UserRequestDto;
 import com.app.api.service.UserService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,8 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
+    @ApiOperation(value = "회원가입", notes = "아이디, 패스워드, 역할을 입력하여 회원가입을 요청합니다.<br>아이디는 4-10자의 소문자+숫자조합 입니다.<br>패스워드는 8-15자의 알파벳 대문자+소문자+숫자 조합입니다.<br>역할은 0(관리자) 혹은 1(유저)로 구분됩니다.")
     @PostMapping("/signup")
     @ResponseStatus(HttpStatus.CREATED)
     public String signUpUser(@RequestBody @Valid UserRequestDto userRequestDto){
@@ -23,6 +26,7 @@ public class UserController {
         return "new user create complete.";
     }
 
+    @ApiOperation(value = "로그인", notes = "아이디, 패스워드를 입력하여 로그인을 시도합니다.<br>로그인에 성공하면 응답헤더에 토큰이 발급됩니다.<br>다음 요청부터 ")
     @PostMapping("/signin")
     public String signInUser(@RequestBody UserRequestDto userRequestDto, HttpServletResponse response){
         userService.signInUser(userRequestDto, response);
