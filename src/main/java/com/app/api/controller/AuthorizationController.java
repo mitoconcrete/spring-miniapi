6b7b8de082895e0545f6ef2b394d8a5db4067582
mitoconcrete <xogns98@gmail.com>
2022-12-15 23:41:42 +0900

    feat: swagger authorization contoller tags and api description attach.

--- src/main/java/com/app/api/controller/AuthorizationController.java
+++ src/main/java/com/app/api/controller/AuthorizationController.java
@@ -2,6 +2,8 @@ package com.app.api.controller;
 
 import com.app.api.dto.response.AuthorizedResponse;
 import com.app.api.service.AuthorizationService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Api(tags = "인증")
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class AuthorizationController {
     private final AuthorizationService authorizationService;
     @GetMapping("/token/renew")
+    @ApiOperation(value = "토큰 재발급", notes = "Refresh Token 을 전달하여 새로운 토큰을 발급 받습니다.")
     public AuthorizedResponse getRenewTokens(HttpServletRequest request){
         return new AuthorizedResponse(HttpStatus.OK, "토큰이 재발급 되었습니다.", authorizationService.getRenewTokens(request));
     }
