8f6f0d584ca49d22a10a275b8a975d05697c6b20
mitoconcrete <xogns98@gmail.com>
2022-12-15 23:37:38 +0900

    fix: response status change.

--- src/main/java/com/app/api/controller/AuthorizationController.java
+++ src/main/java/com/app/api/controller/AuthorizationController.java
@@ -4,19 +4,19 @@ import com.app.api.dto.response.AuthorizedResponse;
 import com.app.api.service.AuthorizationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class AuthorizationController {
     private final AuthorizationService authorizationService;
     @GetMapping("/token/renew")
     public AuthorizedResponse getRenewTokens(HttpServletRequest request){
-        return new AuthorizedResponse(HttpStatus.CREATED, "토큰이 재발급 되었습니다.", authorizationService.getRenewTokens(request));
+        return new AuthorizedResponse(HttpStatus.OK, "토큰이 재발급 되었습니다.", authorizationService.getRenewTokens(request));
     }
 }
