d5fc3a6d2b236dc17ead7c1bb0ae4c5cfd92437d
mitoconcrete <xogns98@gmail.com>
2022-12-24 01:43:15 +0900

    feat: disconnect user-comment connection.

--- src/main/java/com/app/api/entity/User.java
+++ src/main/java/com/app/api/entity/User.java
@@ -26,9 +26,6 @@ public class User extends Timestamped{
     @Enumerated(value = EnumType.STRING)
     private UserRoleEnum role;
 
-    @OneToMany(mappedBy = "user")
-    private final List<Comment> comments = new ArrayList<>();
-
     public User(String username, String password, UserRoleEnum role) {
         this.username = username;
         this.password = password;
